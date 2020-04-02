030523828087ccd62bd5c7e29e2785fd8b8d5503
Jennifer Paquette <jennidemuir@gmail.com>
2020-04-02 21:52:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |item, index|
-    puts "#{index}. #{item}"
+    puts "#{index +1}. #{item}"
   end
 end
