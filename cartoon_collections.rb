0885bc66b00ee29f4017490f1b5a23a9eb044604
Jennifer Paquette <jennidemuir@gmail.com>
2020-04-02 21:42:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.map do |array|
+    puts "Hello #{array}"
+  end
 end
 
 def list_dwarves(array)
